c21d08fa760f2a9536d4259018894b242b514d6f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-26 19:59:50 -0400

    Set up styled components

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Welcome () {
+    return <h1> Welocme to CryptoConsole</h1>
+}
--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,38 @@
 import React, {Component} from 'react';
 import './App.css';
+import Welcome from './WelcomeMessage';
+import styled, {css} from 'styled-components';
+
+// create a div named MyButton
+// styled it with color green
+// if MyButton is passed a props
+// and if this props is named primary
+// apply css rule that follows
+const MyButton = styled.div`
+  color:green;
+  ${props => props.primary && css`
+  color: palevioletred;
+  `}
+`
+// below component TomatoButton will
+// be of type MyButton and will inherit
+// all the styles defined therein
+// and can overwrite these styles
+// with its own set of rules
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 class App extends Component {
   render () {
 
     return (
-      <div>
-        Hello World
-      </div>
+      <>
+      <MyButton> Hello from non-primary! </MyButton>
+      <MyButton primary={"0"}> Hello from primary! </MyButton>
+      <TomatoButton primary={"0"}> Hello from primary! </TomatoButton>
+      </>
     );
   }
 }
