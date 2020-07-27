0e3aedf90b36cdea5220ef03b25593bd0ab2eef2
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-26 21:57:07 -0400

    Change background color. Add in Google font

--- src/App/index.js
+++ src/App/index.js
@@ -3,35 +3,38 @@ import './App.css';
 import Welcome from './WelcomeMessage';
 import styled, {css} from 'styled-components';
 
-// create a div named MyButton
-// styled it with color green
-// if MyButton is passed a props
-// and if this props is named primary
-// apply css rule that follows
-const MyButton = styled.div`
-  color:green;
-  ${props => props.primary && css`
-  color: palevioletred;
-  `}
-`
-// below component TomatoButton will
-// be of type MyButton and will inherit
-// all the styles defined therein
-// and can overwrite these styles
-// with its own set of rules
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
+// // create a div named MyButton
+// // styled it with color green
+// // if MyButton is passed a props
+// // and if this props is named primary
+// // apply css rule that follows
+// const MyButton = styled.div`
+//   color:green;
+//   ${props => props.primary && css`
+//   color: palevioletred;
+//   `}
+// `
+// // below component TomatoButton will
+// // be of type MyButton and will inherit
+// // all the styles defined therein
+// // and can overwrite these styles
+// // with its own set of rules
+// const TomatoButton = styled(MyButton)`
+//   color: tomato;
+//   border-color: tomato;
+// `;
 
 class App extends Component {
   render () {
 
     return (
       <>
-      <MyButton> Hello from non-primary! </MyButton>
+      {/* <MyButton> Hello from non-primary! </MyButton>
       <MyButton primary={"0"}> Hello from primary! </MyButton>
-      <TomatoButton primary={"0"}> Hello from primary! </TomatoButton>
+      <TomatoButton primary={"0"}> Hello from primary! </TomatoButton> */}
+
+      <Welcome/>
+
       </>
     );
   }
